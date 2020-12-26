686b286c074633f2b7d6eb12cff5903c1b17d18f
Thanh Nguyen Ky <thanh.nguyen-ky@klarna.com>
2020-12-26 17:48:54 +0100

    MultiplePage and NavigationBar One problem: when switching around navigation bar, pages would be refreshed and couldnt keep their state.

--- lib/ui/components/cw_bottom_navigation_bar.dart
+++ lib/ui/components/cw_bottom_navigation_bar.dart
@@ -1,41 +1,37 @@
-import 'package:clan_wealth/ui/screens/clan_page.dart';
-import 'package:clan_wealth/ui/screens/wealth_list_page.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const kPageMap = {
-  WealthListPage.pageId: 0,
-  ClanPage.pageId: 1,
-};
-
-class CwBottomNavigationBar extends StatefulWidget {
-  @override
-  _CwBottomNavigationBarState createState() => _CwBottomNavigationBarState();
+class TabItem {
+  final Icon icon;
+  final String label;
+  final Widget page;
+  TabItem({this.icon, this.label, this.page});
 }
 
-class _CwBottomNavigationBarState extends State<CwBottomNavigationBar> {
-  int _selectedIndex = 0;
+class CwBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final Function onSelectTab;
+  final List<TabItem> tabItems;
+
+  const CwBottomNavigationBar(
+      {this.currentIndex, this.onSelectTab, this.tabItems});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 10.0,
-      currentIndex: _selectedIndex,
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(FontAwesomeIcons.wallet),
-          label: 'Your Wealth',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(FontAwesomeIcons.users),
-          label: 'Clan Wealth',
-        ),
-      ],
-      onTap: (int selectedValue) {
-        setState(() {
-          _selectedIndex = selectedValue;
-        });
-      },
+      currentIndex: 0,
+      items: _buildTabItems(),
+      onTap: onSelectTab,
     );
+  }
+
+  _buildTabItems() {
+    print(tabItems.length);
+    return tabItems
+        .map((tab) => BottomNavigationBarItem(
+              icon: tab.icon,
+              label: tab.label,
+            ))
+        .toList();
   }
 }
--- lib/ui/screens/wealth_list_page.dart
+++ lib/ui/screens/wealth_list_page.dart
@@ -1,32 +1,14 @@
-import 'package:clan_wealth/ui/components/cw_bottom_navigation_bar.dart';
 import 'package:clan_wealth/ui/components/wealth_list.dart';
 import 'package:clan_wealth/ui/components/wealth_container.dart';
-import 'package:clan_wealth/ui/screens/wealth_edit_page.dart';
 import 'package:flutter/material.dart';
 
 class WealthListPage extends StatelessWidget {
-  static const String pageId = 'wealth_list_page';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: WealthContainer(
-          body: WealthList(),
-        ),
+    return SingleChildScrollView(
+      child: WealthContainer(
+        body: WealthList(),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => WealthEditPage(),
-            ),
-          );
-        },
-      ),
-      bottomNavigationBar: CwBottomNavigationBar(),
     );
   }
 }
