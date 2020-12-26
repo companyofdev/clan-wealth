9f996468caa25edc50924ec7c06986bb5c91e6d0
Thanh Nguyen Ky <thanh.nguyen-ky@klarna.com>
2020-12-27 00:04:36 +0100

    Add Amplify config and plugin for auth and pinpoint

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,4 +1,50 @@
 const amplifyconfig = ''' {
-    "UserAgent": "aws-amplify-cli/2.0",
-    "Version": "1.0"
+    "analytics": {
+        "plugins": {
+            "awsPinpointAnalyticsPlugin": {
+                "pinpointAnalytics": {
+                    "appId": "2e5587d4451a42ee97a0f865aa39d070",
+                    "region": "eu-west-1"
+                },
+                "pinpointTargeting": {
+                    "region": "eu-west-1"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "eu-west-1:e014f76b-e71f-4be3-883b-002b1102d284",
+                            "Region": "eu-west-1"
+                        }
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                },
+                "PinpointAnalytics": {
+                    "Default": {
+                        "AppId": "2e5587d4451a42ee97a0f865aa39d070",
+                        "Region": "eu-west-1"
+                    }
+                },
+                "PinpointTargeting": {
+                    "Default": {
+                        "Region": "eu-west-1"
+                    }
+                }
+            }
+        }
+    }
 }''';
--- lib/ui/page/clan_wealth_page.dart
+++ lib/ui/page/clan_wealth_page.dart
@@ -1,4 +1,8 @@
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/material.dart';
+import 'package:clan_wealth/amplifyconfiguration.dart';
 
 class ClanWealthPage extends StatefulWidget {
   @override
@@ -6,10 +10,63 @@ class ClanWealthPage extends StatefulWidget {
 }
 
 class _ClanWealthPageState extends State<ClanWealthPage> {
+  bool _amplifyConfigured = false;
+
+  // Instantiate Amplify
+  Amplify amplifyInstance = Amplify();
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+    // Add Pinpoint and Cognito Plugins
+    AmplifyAnalyticsPinpoint analyticsPlugin = AmplifyAnalyticsPinpoint();
+    AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+    amplifyInstance.addPlugin(authPlugins: [authPlugin]);
+    amplifyInstance.addPlugin(analyticsPlugins: [analyticsPlugin]);
+
+    // Once Plugins are added, configure Amplify
+    await amplifyInstance.configure(amplifyconfig);
+    try {
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _recordEvent() async {}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Center(child: Text('Clan Wealth'))),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Amplify Core example app'),
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: [
+                  const Padding(padding: EdgeInsets.all(5.0)),
+                  Text(_amplifyConfigured ? "configured" : "not configured"),
+                  RaisedButton(
+                    // onPressed: _amplifyConfigured ? _recordEvent : null,
+                    child: const Text('record event'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
